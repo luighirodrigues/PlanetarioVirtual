1fa34b9a4a7e182301a15d2a256ecbb2324fd839
Luighi Rodrigues <54685098+luighirodrigues@users.noreply.github.com>
2020-10-26 16:12:23 -0300

    26/10
    Texturizado
    Funcional
    Esperando Layout

---
+++ PlanetarioVirtual/Assets/Scripts/Navegador.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Navegador : MonoBehaviour
+{
+    public Camera origem;
+    public Material material;
+    public Renderer mesh;
+    public TextMesh nome;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(origem.transform.position, origem.transform.forward, out hit))
+        {
+            material = hit.transform.GetComponent<MeshRenderer>().material;
+            if (hit.transform.name.Equals("Display"))
+            {
+                Debug.Log("Display Atingido!");
+            }
+            else
+            {
+                Debug.Log(material.name);
+                nome.text = hit.transform.name;
+                mesh.sharedMaterial = material;
+            }
+        }
+    }
+    
+}
